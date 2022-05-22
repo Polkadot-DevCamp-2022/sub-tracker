69d6fba6600796735dfe971425630df3b466f9cd
Lendrix <lim_lendrix@yahoo.com.sg>
2022-05-22 14:02:49 +0800

    Implemented create_shipment and update_shipment. Reordered type declarations alphabetically.

--- substrate-node-template/pallets/template/src/lib.rs
+++ substrate-node-template/pallets/template/src/lib.rs
@@ -23,10 +23,10 @@
 	#[scale_info(skip_type_params(T))]
 	pub struct Shipment<T: Config> {
 		pub creator: T::AccountId,
-		owner: T::AccountId,
 		pub fees: Option<BalanceOf<T>>,
-		pub status: ShipmentStatus,
+		pub owner_index: u8,
 		pub route: BoundedVec<T::AccountId, T::MaxSize>,
+		pub status: ShipmentStatus,
 	}
 
 	#[derive(Clone, Encode, Decode, PartialEq, Copy, RuntimeDebug, TypeInfo, MaxEncodedLen)]
@@ -66,29 +66,41 @@
 
 	#[pallet::error]
 	pub enum Error<T> {
+		InvalidKey,
 		InvalidRoute,
-		OwnerNotFound,
+		KeyNotFound,
+		ShipmentAlreadyExists,
+		ShipmentKeyAlreadyExists,
 		ShipmentNotFound,
 		TransitPointAlreadyExists,
 		TransitPointNotFound,
 		UnauthorizedCaller,
 	}
 
-	// transit_node -> node_uid map
-	#[pallet::storage]
-	pub(super) type TransitNodes<T:Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		T::AccountId,
-		u8,
-		OptionQuery,
-	>;
-
 	#[pallet::storage]
 	pub(super) type NodeUID<T:Config> = StorageValue<
 		_,
 		u8,
 		ValueQuery,
+	>;
+
+	// key -> shipment_uid map
+	#[pallet::storage]
+	pub(super) type ShipmentsKeyMap<T:Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		[u8; 16],
+		u64,
+		OptionQuery,
+	>;
+
+	// shipment_uid -> shipment map
+	#[pallet::storage]
+	pub(super) type Shipments<T:Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		u64,
+		Shipment<T>,
 	>;
 
 	#[pallet::storage]
@@ -98,12 +110,14 @@
 		ValueQuery,
 	>;
 
+	// transit_node -> node_uid map
 	#[pallet::storage]
-	pub(super) type Shipments<T:Config> = StorageMap<
+	pub(super) type TransitNodes<T:Config> = StorageMap<
 		_,
 		Blake2_128Concat,
-		u64,
-		Shipment<T>,
+		T::AccountId,
+		u8,
+		OptionQuery,
 	>;
 
     #[pallet::call]
@@ -149,32 +163,51 @@
 
 			let shipment = Shipment::<T> {
 				creator: transit_node.clone(),
-				owner: route_vec.get(1).unwrap().clone(),
 				fees: None, // Todo: Calculate fees based on the route
-				status: ShipmentStatus::InTransit,
-				route: route_vec
+				owner_index: 1,
+				route: route_vec,
+				status: ShipmentStatus::InTransit
 			};
 
-			//create Restricted Key => Create a type first so the shipment uid maps to the type (TO-DO)
-			// Todo: store shipment (to map?)
+			ensure!(!Shipments::<T>::contains_key(&new_uid), Error::<T>::ShipmentAlreadyExists);
+			Shipments::<T>::insert(&new_uid, &shipment);
+
+			let key = Self::gen_key(); // Todo: How will next destination know/get this key value?
+			ensure!(!ShipmentsKeyMap::<T>::contains_key(&key), Error::<T>::ShipmentKeyAlreadyExists); // Todo: Is this check necessary?
+			ShipmentsKeyMap::<T>::insert(&key, &new_uid);
 
 			Self::deposit_event(Event::ShipmentCreated(transit_node));
 			Ok(())
 		}
 
 		#[pallet::weight(0)]
-		pub fn update_shipment(origin: OriginFor<T>, uid: u64) -> DispatchResult {
-			// This function will take a key parameter but we
-			// don't know what the type will be. I'm working on it
+		pub fn update_shipment(origin: OriginFor<T>, uid: u64, key: [u8; 16]) -> DispatchResult {
+
 			let transit_node = ensure_signed(origin)?;
 			ensure!(TransitNodes::<T>::contains_key(&transit_node), Error::<T>::UnauthorizedCaller);
+			ensure!(ShipmentsKeyMap::<T>::contains_key(&key), Error::<T>::KeyNotFound);
+			ensure!(ShipmentsKeyMap::<T>::get(&key).unwrap() == uid, Error::<T>::InvalidKey);
+			ensure!(Shipments::<T>::contains_key(uid), Error::<T>::ShipmentNotFound);
 
-			// Match the input key with the one stored on the blockchain (Not implemented yet. see the first comment in this function)
+			let mut shipment = Shipments::<T>::get(uid).unwrap();
+			ensure!(&transit_node == shipment.route.get(shipment.owner_index as usize).unwrap(), Error::<T>::UnauthorizedCaller);
+			ShipmentsKeyMap::<T>::remove(&key);
 
-			// Check if the current transit node is the destination. If it is,  change the status of the shipment.
-
-			// Otherwise , generate a new key and a new owner
-
+			match shipment.owner_index == shipment.route.len() as u8 - 1 {
+				true => {
+					// Shipment has reached end destination
+					shipment.status = ShipmentStatus::Delivered;
+					Self::deposit_event(Event::ShipmentReceived(transit_node));
+				},
+				false => {
+					// Shipment is still in transit
+					shipment.owner_index = shipment.owner_index + 1;
+					let new_key = Self::gen_key(); // Todo: How will next destination know/get this key value?
+					ensure!(!ShipmentsKeyMap::<T>::contains_key(&new_key), Error::<T>::ShipmentKeyAlreadyExists); // Todo: Is this check necessary?
+					ShipmentsKeyMap::<T>::insert(&new_key, &uid);
+					Self::deposit_event(Event::ShipmentUpdated(transit_node));
+				}
+			}
 			Ok(())
 		}
 	}
@@ -182,7 +215,7 @@
 	// Helpful functions
 	impl<T: Config> Pallet<T> {
 
-		fn gen_key(&self) -> [u8; 16] {
+		fn gen_key() -> [u8; 16] {
 			let payload = (
 				T::KeyRandomNess::random(&b"key"[..]).0,
 				<frame_system::Pallet<T>>::extrinsic_index().unwrap_or_default(),
